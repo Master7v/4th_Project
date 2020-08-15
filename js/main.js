5d75b901df5c85d2ae75927c3994eb496f6ac69e
M7V <54369047+Master7v@users.noreply.github.com>
2020-08-15 20:20:45 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/*
+ * Auto-generated content from the Brackets New Project extension.
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global $, window, document */
+
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        nav: true,
+        dots:false,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 2
+            }
+        }
+
+    });
+});
